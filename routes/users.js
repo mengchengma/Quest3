8757f5ee798244478014a23bf4559084339196db
mengchengma <mengchengma@gmail.com>
2025-11-12 14:00:22 -0500

    id params

--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,25 @@ router.get('/new', (req, res) => {
     res.send("New User Form");
 });
 
+// router.post('/', (req, res) => {
+//     res.send("Create New User");
+// });
+
+// router.get('/:id', (req, res) => {
+//     res.send(`Getting User Data: ${req.params.id}`);
+// });
+
+router.param("id", (req, res, next, id) => {
+    console.log(`User ID is: ${id}`);
+    next();
+});
+
+router.route('/:id').get((req, res) => {
+    res.send(`Getting User Data: ${req.params.id}`);
+}).delete((req, res) => {
+    res.send(`Deleting User Data: ${req.params.id}`);
+}).put((req, res) => {
+    res.send(`Updating User Data: ${req.params.id}`);
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -6,17 +6,12 @@ app.set('view engine', 'ejs');
 const postsRouter = require('./routes/posts');
 app.use('/posts', postsRouter);
 
+const usersRouter = require('./routes/users');
+app.use('/users', usersRouter);
+
 app.get('/', (req, res) => {
     console.log('Hello World');
     res.render("index", {user: "Mengs"});
-});
-
-app.get('/users', (req, res) => {
-    res.send("Users List");
-});
-
-app.get('/users/new', (req, res) => {
-    res.send("New User Form");
 });
 
 app.listen(3032);
